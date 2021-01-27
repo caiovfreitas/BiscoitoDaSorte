437c31bddba0abece8c0ca7cd9c993347ef08162
Caio Freitas <cvaf@cin.ufpe.br>
2021-01-26 21:59:30 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, Alert } from 'react-native';
+
+class App extends Component{
+
+  render(){
+    return(
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default App;
